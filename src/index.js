6cd6e20fd585cb83edc2e3b37323c54ce6110546
Javier Cervelló <javi.cervello@gmail.com>
2019-10-24 11:51:03 +0200

    redux dependences imported in index.js and Provider setted

--- src/index.js
+++ src/index.js
@@ -3,10 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+//Importamos esto como primer paso para implementar redux.
+// Dependecias de redux
+import {createStrore} from 'redux'
+import {Provider} from 'react-redux'
+// Creamos el store a partir de un conjunto de Reducers
+const store = createStrore();
+ReactDOM.render(
+    //Provaider SIEMPRE tiene el Atributo/prop  strore.
+<Provider store={store}>
+    
+    <App/>
 
-ReactDOM.render(<App />, document.getElementById('root'));
+</Provider>
+,document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+
 serviceWorker.unregister();
